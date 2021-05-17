36e3c9f5a95622559434fcabba7c152ccb36482f
Saba <68478947+codersaba@users.noreply.github.com>
2021-05-17 12:00:52 +0530

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,110 @@
+class Game{
+    constructor(){
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+
+    player2 = createSprite(800,500);
+
+    players=[player1,player2];
+
+        }
+    
+    play(){
+        
+                form.hide();
+
+                Player.getPlayerInfo();
+                
+                 var x =100;
+                 var y=200;
+                 var index =0;
+                 drawSprites();
+                 for(var plr in allPlayers){
+                    
+                    
+                     index = index+1;
+                     x = 500-allPlayers[plr].distance;
+                     y=500;
+                     
+                     players[index -1].x = x;
+                     players[index - 1].y = y;
+                       
+                     if(index === player.index){
+                         
+                         fill("black");
+                         textSize(25);
+                         text(allPlayers[plr].name ,x-25,y+25);
+
+                         
+                     }
+                    
+                         textSize(25);
+                         fill("white");
+                         text("Player 1 :" +allPlayers.player1.score,50,50);
+                        text("Player 2 :" + allPlayers.player2.score, 50, 100);
+                 
+                 }
+                
+                
+                 
+
+                if (keyIsDown(RIGHT_ARROW) && player.index !== null) {
+                    player.distance -= 10
+                    player.update();
+                }
+                if (keyIsDown(LEFT_ARROW) && player.index !== null) {
+                    player.distance += 10
+                    player.update();
+                }
+            
+                
+                 
+                  if (player.index !== null) {
+                     
+                         
+                              
+                              player.score =player.score+1;
+                              player.update();
+                              
+                          
+                          
+                      
+                  }
+                
+
+         
+         
+        
+         
+
+    }
+
+    end(){
+       console.log("Game Ended");
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,42 @@
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+var score =0;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+
+
+var player1score =0;
+var player2score =0;
+
+function preload(){
+  
+}
+function setup() {
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  background(0);
+  
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+    
+     game.end();
+   }
+}
